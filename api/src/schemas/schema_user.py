24fc798a90c9daa5230cceec92ed0c87cf9cbb07
jesustr20 <jesus.tr61@gmail.com>
2024-04-26 23:55:01 -0500

    order attributes in api user/profile

--- api/src/schemas/schema_user.py
+++ api/src/schemas/schema_user.py
@@ -1,16 +1,20 @@
 from marshmallow.fields import Enum, Nested
+from marshmallow import fields
 from src.schemas.schema import ma
 from src.model.model_user import UserModel
 from src.entities.persontype_enum import PersonType
 from src.schemas.schema_profile import ProfileSchema
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
+    
     class Meta:
         model = UserModel
         load_instance = True
-
+        include_relationships = True        
+        fields = ('id', 'username', 'age', 'email', 'category', 'profile')
+    
     category = Enum(PersonType, by_value=True, required=False)
-    propfile = Nested(ProfileSchema)
+    profile = fields.Nested(ProfileSchema, many=True)
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
